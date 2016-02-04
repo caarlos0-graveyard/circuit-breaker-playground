23831eb8c715f8442fa9552c33dbca3026199ed1
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-02-04 17:55:17 -0200

    api

---
+++ api/src/main/java/com/carlosbecker/circuit/api/ApiApplication.java
@@ -0,0 +1,9 @@
+package com.carlosbecker.circuit.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+@SuppressWarnings("EmptyClass")
+public class ApiApplication extends Application {
+}
---
+++ api/src/main/java/com/carlosbecker/circuit/api/Apiresource.java
@@ -0,0 +1,17 @@
+package com.carlosbecker.circuit.api;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("calc")
+public class ApiResource {
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{number}")
+    public int calc(@PathParam("number") final int number) {
+        return number * 20;
+    }
+}
