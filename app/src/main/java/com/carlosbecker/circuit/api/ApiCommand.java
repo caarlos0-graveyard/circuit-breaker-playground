0a1b82d8167fa9c7a52008d4a3270658cedcbed6
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-02-04 18:58:05 -0200

    all working

---
+++ app/src/main/java/com/carlosbecker/circuit/api/ApiCommand.java
@@ -0,0 +1,35 @@
+package com.carlosbecker.circuit.api;
+
+import com.jcabi.http.Request;
+import com.jcabi.http.request.JdkRequest;
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+public final class ApiCommand extends HystrixCommand<Integer> {
+    private final transient int number;
+
+    public ApiCommand(final int number) {
+        super(HystrixCommandGroupKey.Factory.asKey("API"));
+        this.number = number;
+    }
+
+    @Override
+    protected Integer run() throws Exception {
+        final String result = new JdkRequest("http://localhost:8080/api")
+            .uri().path("/calc").path(String.format("/%d", this.number)).back()
+            .method(Request.POST)
+            .fetch()
+            .body();
+        return Integer.parseInt(result);
+    }
+
+    @Override
+    protected String getCacheKey() {
+        return String.valueOf(this.number);
+    }
+
+    @Override
+    protected Integer getFallback() {
+        return -1;
+    }
+}
--- app/src/main/java/com/carlosbecker/circuit/api/AppResource.java
+++ app/src/main/java/com/carlosbecker/circuit/api/AppResource.java
@@ -1,5 +1,7 @@
 package com.carlosbecker.circuit.api;
 
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
+import javax.annotation.PreDestroy;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -8,11 +10,21 @@ import javax.ws.rs.core.MediaType;
 
 @Path("calc")
 public class AppResource {
+    private final transient HystrixRequestContext context;
+
+    public AppResource() {
+        this.context = HystrixRequestContext.initializeContext();
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        this.context.shutdown();
+    }
+
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/{number}")
     public int calc(@PathParam("number") final int number) {
-//        HystrixCommand command = new HystrixCommand();
-        return number * 20;
+        return new ApiCommand(number).execute();
     }
 }
