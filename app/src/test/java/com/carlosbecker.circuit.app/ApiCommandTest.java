e49914880f9fdeb29b7b497f2e47206638b09246
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-02-05 14:01:06 -0200

    ignores

--- app/src/test/java/com/carlosbecker.circuit.app/ApiCommandTest.java
+++ app/src/test/java/com/carlosbecker.circuit.app/ApiCommandTest.java
@@ -2,10 +2,12 @@ package com.carlosbecker.circuit.app;
 
 import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public final class ApiCommandTest {
     @Test
+    @Ignore
     public void test() {
         HystrixRequestContext context = HystrixRequestContext.initializeContext();
         try {
@@ -17,6 +19,7 @@ public final class ApiCommandTest {
         } finally {
             context.shutdown();
         }
+        // this will fail, the caching only works in the same request context...
         context = HystrixRequestContext.initializeContext();
         try {
             final ApiCommand cmd = new ApiCommand(2);
